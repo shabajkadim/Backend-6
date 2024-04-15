0fe0fc25ae9dc3990a5816cbb0d2a79ae3292e98
Shahabaj kadim <shahabajkadim7@gmail.com>
2024-04-15 23:56:21 +0530

    updates

---
+++ Day-5/index.js
@@ -0,0 +1,29 @@
+import express, { json } from "express";
+
+const app=express()
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("welcome")
+})
+
+app.post("/register",(req,res)=>{
+    // res.send("register page")
+    try{
+        const{name,email,password,confirmPassword}=req.body
+        if(!name || !email || !password || !confirmPassword){
+            res.send("All Feilds are required")
+        }
+        if(password !== confirmPassword){
+            res.send("check password nad confirmPassword")
+        }
+
+        res.send("Register Sucessfull")
+    }catch(error){
+        res.send("error")
+    }
+})
+
+app.listen(8000,(()=>{
+    console.log("port running on 8000");
+}))
